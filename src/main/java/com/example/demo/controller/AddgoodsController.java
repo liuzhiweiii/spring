8d29aabc2aa8cbeb424e28d3a65c5daea4a13c6c
Erineu <2304890480@qq.com>
2024-12-23 22:00:14 +0800

    test

--- src/main/java/com/example/demo/controller/AddgoodsController.java
+++ src/main/java/com/example/demo/controller/AddgoodsController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;//package com.example.demo.controller;
 //
 import com.example.demo.common.Result;
-import com.example.demo.from.CreateGoodsForm;
 import com.example.demo.service.AddgoodsService;
 import com.example.demo.untity.Goods;
 import io.swagger.annotations.ApiOperation;
@@ -38,21 +37,21 @@ public class AddgoodsController {
     @Autowired
     private AddgoodsService addgoodsService;
 
-//    @ApiOperation("新增商品")
-//    @PostMapping("/add") // 只定义具体接口路径
-//    public Result addGoods(@RequestBody Goods addGoods) {
-//        System.out.println("接收到的商品数据: " + addGoods.toString());
-//        addgoodsService.addGoods(addGoods);
-//        return Result.ok().message("商品添加成功");
-//    }
-
-
     @ApiOperation("新增商品")
-    @PostMapping("/add")
-    public Result addGoods(@RequestBody CreateGoodsForm form) {
-        System.out.println("123");
-        System.out.println("接收到的商品数据: " + form);
+    @PostMapping("/add") // 只定义具体接口路径
+    public Result addGoods(@RequestBody Goods addGoods) {
+        System.out.println("接收到的商品数据: " + addGoods.toString());
+        addgoodsService.addGoods(addGoods);
         return Result.ok().message("商品添加成功");
     }
+
+
+//    @ApiOperation("新增商品")
+//    @PostMapping("/add")
+//    public Result addGoods(@RequestBody CreateGoodsForm form) {
+//        System.out.println("123");
+//        System.out.println("接收到的商品数据: " + form);
+//        return Result.ok().message("商品添加成功");
+//    }
 
 }
